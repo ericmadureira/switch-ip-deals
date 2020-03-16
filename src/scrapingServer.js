26177383575b241f8086fd20aff511be62267dfc
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-03-15 21:06:40 -0300

    feat: scraping server
    NodeJS scraping server which is called by the react client and
    redirects the result avoiding CORS problems.

--- src/scrapingServer.js
+++ src/scrapingServer.js
@@ -1,17 +1,33 @@
 import express from 'express';
-import axios from 'axios';
-import baseContentUrl from './services/constants';
 import cors from 'cors';
+import puppeteer from 'puppeteer';
+import { baseContentUrl } from './services/constants';
 
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
-app.get('/:gameName', async (req, res) => {
-  const gameName = req.params.gameName;
-  const url = `${baseContentUrl}/${gameName}`;
-  const response = await axios.get(url);
-  res.send(response.data);
+const spaFetch = async (url) => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector('.body-2', { timeout: 10000 });
+    const result = await page.evaluate(() => {
+      const body = document.querySelector('body').innerHTML;
+      return body;
+    });
+    await browser.close();
+    return result;
+  } catch (error) {
+    console.log(error)
+   }
+}
+
+app.get('/:gameUrl', async (req, res) => {
+  const gameUrl = req.params.gameUrl;
+  const htmlBody = await spaFetch(`${baseContentUrl}/${gameUrl}`);
+  res.send(htmlBody);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

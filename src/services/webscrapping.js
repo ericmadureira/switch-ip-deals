d096bc7fe3f12217942acd3f8c756ae0d0ecd7ac
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-03-15 21:04:01 -0300

    feat: client scrape
    Module responsible for calling the scraping API.

--- src/services/webscrapping.js
+++ src/services/webscrapping.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const getScrapedGame = async () => {
-  const gameName = 'the-legend-of-zelda-breath-of-the-wild';
-  const response = await axios.get(`http://localhost:5000/${gameName}`);
-  const { data } = response;
-  return data;
+const getScrapedGame = async (gameUrl) => {
+  const response = await axios.get(`http://localhost:5000/${gameUrl}`);
+  return response.data;
 };
 
 export default getScrapedGame;

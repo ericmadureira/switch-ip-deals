47397806b7dadfdd5e9cddafb1d75d8a59f6847c
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-03-19 20:52:24 -0300

    feat: add improvements to constants
    1 more game, primary colors for the cards and url helper.

--- src/services/constants.js
+++ src/services/constants.js
@@ -4,5 +4,18 @@ export const gamesInfos = [
   {
     name: 'Legend of Zelda: Breath of the Wild',
     url: 'the-legend-of-zelda-breath-of-the-wild',
+    primaryColor: '#3CADD5',
+  },
+  {
+    name: "Luigi's Mansion 3",
+    url: 'luigi-s-mansion-3',
+    primaryColor: '#73E831',
+  },
+  {
+    name: 'Super Mario Odyssey',
+    url: 'super-mario-odyssey',
+    primaryColor: '#F1452F',
   },
 ];
+
+export const gameBuyUrl = (url) => `${baseContentUrl}/${url}`;

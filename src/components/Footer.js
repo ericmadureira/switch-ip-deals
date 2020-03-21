4f4983eedaff4c1e0e559ffba5e094ea77e7e72e
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-03-21 20:40:21 -0300

    feat: fix footer link style
    Add target blank and change link style.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import styles from './Footer.css';
 
 const Footer = () => (
   <footer className='links'>
-    <a href='https://github.com/ericmadureira/switch-ip-deals'>Project source</a>
+    <a
+      href='https://github.com/ericmadureira/switch-ip-deals'
+      rel='noopener noreferrer'
+      target='_blank'
+    >Project source</a>
   </footer>
 );
 
